f78f81eac6d84097f715116ddd3ba3c9e96d0cf6
guilhermeluis00 <luisguilhermesaraivafeitosa@gmail.com>
2026-04-12 21:35:02 -0300

    Primeiro COmmit

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+#Configuraçao do FASTAPI
+app = FastAPI(
+    title="Teste API",
+    description="API para teste de autenticação e gerenciamento de usuários",
+)
+#Criação da Rota de teste
+@app.get("/")
+def root():
+    return {"message": "Bem-vindo à API de teste!"}
